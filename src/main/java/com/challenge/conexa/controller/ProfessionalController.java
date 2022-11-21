1b44c585563f8624fd5512e19559de0ed6a458d1
Daniel Ramos <danielrrbhmg@gmail.com>
2022-11-21 20:12:57 -0300

    queryProfessionalFilter

--- src/main/java/com/challenge/conexa/controller/ProfessionalController.java
+++ src/main/java/com/challenge/conexa/controller/ProfessionalController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,12 @@ public class ProfessionalController {
     @GetMapping(value = "/findAll")
     public ResponseEntity<List<Professional>> findPatients() {
         return ResponseEntity.ok().body(professionalService.findAll());
+    }
+
+    @CrossOrigin(origins = ip)
+    @GetMapping(value = "/search/{key}")
+    public ResponseEntity<List<Professional>> search(@PathVariable String key) {
+        return ResponseEntity.ok().body(professionalService.search(key));
     }
    
 }
--- src/main/java/com/challenge/conexa/repository/ProfessionalRepository.java
+++ src/main/java/com/challenge/conexa/repository/ProfessionalRepository.java
@@ -3,6 +3,7 @@ package com.challenge.conexa.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +16,12 @@ public interface ProfessionalRepository extends CrudRepository<Professional, Lon
     Optional<Professional> findById(Long professionalId);
 
     Optional<Professional> findByLogin(String login);
+
+    @Query(value = "select * from professional p "+
+    "inner join users u on p.id = u.id "+
+    "where name like %:key% or "+
+    "crm like %:key%  or "+
+    "status like %:key%",
+    nativeQuery = true)
+    List<Professional> search(String key);
 }
